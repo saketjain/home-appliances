13c65dc0b0f4ac630bee8497dde80bc605ac0b22
Anshuman Kumar <anshuman.kumar@voicetree.co>
2018-08-15 10:39:24 +0530

    Added template for neighbourhood app

--- src/app.tsx
+++ src/app.tsx
@@ -4,6 +4,7 @@ import { AppHome } from './app-home';
 import { configStore } from './features/config';
 import { contextMenuStore } from './features/navigation/stores/context-menu-store';
 import { drawerMenuStore } from './features/navigation/stores/drawer-menu-store';
+import { mainContainerStore } from './features/navigation/stores/main-container-store';
 import { topNavStore } from './features/navigation/stores/top-nav-store';
 import Login from './features/security/components/login';
 import { authStore } from './features/security/stores/auth-store';
@@ -12,13 +13,14 @@ import { authStore } from './features/security/stores/auth-store';
 export class App extends React.Component {
 
     public render () {
-        const stores = {
+        const stores = { 
             authStore,
             configStore,
             contextMenuStore,
             drawerMenuStore,
-            topNavStore
-          };
+            mainContainerStore,
+            topNavStore 
+        };
 
         return (
             <Provider {...stores}>
