c3ae3c87ad575da1f276146b64b020feaf4ae6d4
Saket Jain <sakjain@publicisgroupe.net>
2018-12-01 22:52:24 +0530

    Improved UI

--- src/features/neighbourhood-simulation/components/scene/sunYellow.tsx
+++ src/features/neighbourhood-simulation/components/scene/sunYellow.tsx
@@ -1,11 +1,10 @@
 import createStyles from '@material-ui/core/styles/createStyles';
 import withStyles, { WithStyles } from '@material-ui/core/styles/withStyles';
 import * as React from 'react';
-import * as ReactCSSTransitionGroup from 'react-addons-css-transition-group'; // ES6
 import sun from '../../../../img/sun.png';
 
 const styles = createStyles({
-    sunYellow: {
+    sunStart: {
         position: 'absolute',
         left: '45%',
         top: '50%',
@@ -15,6 +14,19 @@ const styles = createStyles({
         height: '152px',
         background: 'transparent no-repeat center center',
         zIindex: 2
+    },
+    sunEnd: {
+        position: 'absolute',
+        left: '45%',
+        top: '50%',
+        right: '0px',
+        bottom: '0px',
+        width: '150px',
+        height: '152px',
+        background: 'transparent no-repeat center center',
+        zIindex: 2,
+        transition: 'transform 5s ease-in-out',
+        transform: 'translate(0px, 100px)'
     }
 });
 
@@ -22,14 +34,32 @@ interface ISunYellowProps extends WithStyles<typeof styles> {
     time?: number; 
 }
 
-class SunYellow extends React.Component<ISunYellowProps> {
+interface ISunYellowState {
+    startAnimation: boolean;
+}
+
+class SunYellow extends React.Component<ISunYellowProps, ISunYellowState> {
     
+    constructor(props: ISunYellowProps) {
+        super(props);
+        this.state = {
+            startAnimation: false
+        };
+    }
+
+    public componentWillMount() {
+        setTimeout(() => {
+            this.setState({
+                startAnimation: true
+            })
+        }, 1000);
+    }
+
     public render() {
         const { classes } = this.props;
+        const styleClass = this.state.startAnimation ? classes.sunEnd : classes.sunStart
         return (
-            <ReactCSSTransitionGroup transitionName="sunYellow">
-                <img className = {classes.sunYellow} src={sun}/>
-            </ReactCSSTransitionGroup>
+            <img className = {styleClass} src={sun}/>
         )
     }
 }
